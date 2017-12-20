b438ea3837bb26418151829312b773e8331aa151
yevhen.zhuravel <yevhen.zhuravel@dev-pro.net>
2017-12-20 11:02:53 +0200

    interaction components

--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Input, Component } from '@angular/core';
 
 @Component({
     selector: 'child-comp',
-    template: `<ng-content></ng-content>
-                <p>Привет {{name}}</p>`,
-    styles: [`h2, p {color:navy;}`]
+    template: `<p>Имя пользователя: {{userName}}</p>
+              <p>Возраст пользователя: {{userAge}}</p>`
 })
 export class ChildComponent {
-    name = "Евгений";
+    @Input() userName: string;
+    _userAge: number;
+
+    @Input()
+    set userAge(age: number) {
+        if (age < 0)
+            this._userAge = 0;
+        else if (age > 100)
+            this._userAge = 100;
+        else
+            this._userAge = age;
+    }
+    get userAge() { return this._userAge; }
 }
