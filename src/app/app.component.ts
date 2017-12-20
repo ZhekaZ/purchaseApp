c69d2eb7c12ec18f04a3d91b7c3dd514bd3eaa4b
yevhen.zhuravel <yevhen.zhuravel@dev-pro.net>
2017-12-20 14:08:38 +0200

    double-side binding for multi child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<child-comp [(userName)]="name"></child-comp>
-                <div>Выбранное имя: {{name}}</div>`
+    template: `<child-comp [(userName)]="name" [(userSurName)]="surName"></child-comp>
+                <div>Выбранное имя: {{name}}</div>
+                <div>Выбранное фамилия: {{surName}}</div>
+              `
 })
 export class AppComponent {
     name: string = "Tom";
+    surName: string = 'Johnson';
 }
--- src/app/child.component.ts
+++ src/app/child.component.ts
@@ -2,15 +2,24 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'child-comp',
-    template: `<input [ngModel]="userName" (ngModelChange)="onNameChange($event)" />`
+    template: `<input [ngModel]="userName" (ngModelChange)="onNameChange($event)" />
+               <input [ngModel]="userSurName" (ngModelChange)="onSurNameChange($event)"/>
+              `
 })
 export class ChildComponent {
-
     @Input() userName: string;
     @Output() userNameChange = new EventEmitter<string>();
-    onNameChange(model: string) {
 
+    onNameChange(model: string) {
         this.userName = model;
         this.userNameChange.emit(model);
+    }
+
+    @Input() userSurName: string;
+    @Output() userSurNameChange = new EventEmitter<string>();
+
+    onSurNameChange(model: string) {
+        this.userSurName = model;
+        this.userSurNameChange.emit(model);
     }
 }
